fedad0b34fb10952ba467291f77766f75eecf795
Salih Yılmaz <salih_yil@live.com>
2021-05-08 19:23:12 +0300

    3 : Variables ( Değişkenler )

---
+++ jscript.js
@@ -0,0 +1,18 @@
+//console.log('Arin');
+
+
+
+var name = 'Salih';
+var age = 3;
+var job;
+job =  "doktor";
+var erkek = false;
+
+console.log(erkek);
+console.log(name);
+console.log(age1); //jsde age1 hatasını aldıktan sonra bu koodan sonraki diğer hataları göstermez. console.log(Age); consola gelmez.
+console.log(job);
+
+
+//js büyük küçük harfe duyarlıdır.
+console.log(Age); //Age is not defined hatası verir.
